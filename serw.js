83a92d19a5db79e33b7cde3f4275d53b4f95a6de
A. Alper Atıcı <alper.goplay@gmail.com>
2022-10-18 12:04:33 +0300

    PWA

---
+++ serw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('c-damalper')
+    .then(c => c.addAll(['./index.html', './css/style.css',
+                         './img/icons.svg', './img/tahta.svg',
+                         './img/favicon-192.png', './img/favicon.ico', './img/ikon.png', './img/ikon-apple.png',
+                         './js/dama.js', './js/gorsel.js']))
+  );
+});
+
+self.addEventListener("activate", e => console.log("serw activate."));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open('c-damalper').then(c => c.match(e.request))
+                             .then(resp => resp ?? fetch(e.request))
+  );
+});
