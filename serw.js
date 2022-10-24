1c64752dcbdfae5164860a143f5e8d92cf7c23b2
A. Alper Atıcı <alper.goplay@gmail.com>
2022-10-24 12:38:07 +0300

    önce netw, sonra cache.

--- serw.js
+++ serw.js
@@ -1,18 +1,16 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('c-damalper')
-    .then(c => c.addAll(['./index.html', './css/style.css',
-                         './img/icons.svg', './img/tahta.svg',
-                         './img/favicon-192.png', './img/favicon.ico', './img/ikon.png', './img/ikon-apple.png',
-                         './js/dama.js', './js/gorsel.js']))
-  );
-});
+self.addEventListener('install', () => console.log('serw install.'));
 
-self.addEventListener("activate", e => console.log("serw activate."));
+self.addEventListener("activate", () => console.log('serw activate.'));
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.open('c-damalper').then(c => c.match(e.request))
-                             .then(resp => resp ?? fetch(e.request))
+    caches.open('c-damalper').then(c => {
+      return fetch(e.request)
+              .then(resp => {
+                  c.put(e.request, resp.clone());
+                  return resp;
+               })
+              .catch(() => c.match(e.request));
+      })
   );
 });
